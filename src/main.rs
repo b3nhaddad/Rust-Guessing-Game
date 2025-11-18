d7a3e1b255902ff2afd1633a3bd6d73f16f2930b
Benjamin Haddad <couchroomkid@Benjamins-MacBook-Air-6.local>
2025-11-18 08:39:45 -0700

    initialize repo (final commit)

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+	println!("Guessing game");
+        
+        let minimum = 1;
+        let maximum = 10;
+
+        let secret_num = rand::thread_rng().gen_range(minimum..=maximum);
+
+        
+        println!("Secret number is between range {minimum} to {maximum}"); 
+
+        guess(secret_num);
+
+	println!("You got it. Answer {secret_num}");
+}
+
+fn guess(secret_number : u32){
+    
+        
+	println!("Please Input Your Guess.");
+	let mut guess1 = String::new();
+        
+	std::io::stdin()
+		.read_line(&mut guess1)
+		.expect("Failed to read line");
+
+        let guess1: u32 = match guess1.trim().parse(){
+            Ok(num) => num,
+            Err(_) => 0, 
+        }; 
+        match guess1.cmp(&secret_number){
+        
+            Ordering::Less => {println!("WRONG!"); guess(secret_number);}, 
+            Ordering::Greater => {println!("WRONG!"); guess(secret_number);}, 
+            Ordering::Equal => println!("Hurray Huzz!"),
+        }
+}
